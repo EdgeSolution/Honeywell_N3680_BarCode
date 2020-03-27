69aaa3c9eed345db1ddc58923299844b459205bb
fengchao.dai(代锋超) <fengchao.dai@advantech.com.cn>
2020-03-27 15:50:38 +0800

    Fixed the bug that app cannot connect to the broker in android8 system.

--- Libraries/hedc_android/ScanBarCode/src/main/java/com/adv/scanbarcode/MainActivity.java
+++ Libraries/hedc_android/ScanBarCode/src/main/java/com/adv/scanbarcode/MainActivity.java
@@ -2,6 +2,7 @@ package com.adv.scanbarcode;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +18,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 
 import honeywell.hedc.EngineCommunication;
 import honeywell.hedc_usb_com.HEDCUsbCom;
@@ -63,10 +65,18 @@ public class MainActivity extends Activity implements HEDCUsbCom.OnConnectionSta
                     try {
                         //String command = "ps |grep mosquitto";
                         String command = "ps";
-                        if (commandIsRuning(command)) {
-                            break;
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            String mosquittoIsRunning = getSystemStringProperties(MainActivity.this,"adv.mosquittoIsRunning","false");
+                            if (mosquittoIsRunning != null && !mosquittoIsRunning.isEmpty() && mosquittoIsRunning.equals("true")) {
+                                break;
+                            }
+                            Log.d(TAG, "isServiceRunning ...");
+                        }else {
+                            if (commandIsRuning(command)) {
+                                break;
+                            }
+                            Log.d(TAG, "ps command ...");
                         }
-                        Log.d(TAG, "ps command ...");
                         Thread.sleep(3000);
                     } catch (InterruptedException | IOException e) {
                         Log.d(TAG, "Exception Error ...");
@@ -189,5 +199,28 @@ public class MainActivity extends Activity implements HEDCUsbCom.OnConnectionSta
             e.printStackTrace();
             return false;
         }
+    }
+
+    public static String getSystemStringProperties(Context context, String key, String def) throws IllegalArgumentException {
+        String ret = def;
+        try {
+            ClassLoader cl = context.getClassLoader();
+            @SuppressWarnings("rawtypes")
+            Class SystemProperties = cl.loadClass("android.os.SystemProperties");
+            @SuppressWarnings("rawtypes")
+            Class[] paramTypes = new Class[2];
+            paramTypes[0] = String.class;
+            paramTypes[1] = String.class;
+            Method get = SystemProperties.getMethod("get", paramTypes);
+            Object[] params = new Object[2];
+            params[0] = new String(key);
+            params[1] = new String(def);
+            ret = (String) get.invoke(SystemProperties, params);
+        } catch (IllegalArgumentException iAE) {
+            throw iAE;
+        } catch (Exception e) {
+            ret = def;
+        }
+        return ret;
     }
 }
